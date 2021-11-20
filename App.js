67ba80edc116146b486eda4a5c0d2fde53d264c5
Idris Bankole <bankoleidris@gmail.com>
2021-11-20 17:45:01 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [change, setChange] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {!change ? "shaun" : "riley"}</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>lorem ipsum dolor sit amet</Text>
+        <Text>lorem ipsum dolor sit amet</Text>
+        <Text>lorem ipsum dolor sit amet</Text>
+      </View>
       <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="change name"
+          color="#841584"
+          onPress={() => {
+            setChange(!change);
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +32,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+    marginTop: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 30,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
