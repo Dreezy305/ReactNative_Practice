da061c300558465729c2a1271c968aec7b64577a
Idris Bankole <bankoleidris@gmail.com>
2021-11-21 00:27:51 +0100

    todo app

--- App.js
+++ App.js
@@ -1,43 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { id: 1, name: "shaun" },
-    { id: 2, name: "yoshi" },
-    { id: 3, name: "mario" },
-    { id: 4, name: "luigi" },
-    { id: 5, name: "android" },
-    { id: 6, name: "window" },
-    { id: 7, name: "mac" },
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: 1 },
+    { text: "create an app", key: 2 },
+    { text: "create reducer", key: 3 },
   ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.id !== id);
-    });
-  };
-
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        numColumns={2}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      {/* HEADER GOES HERE */}
+      <View style={styles.header}>
+        <Text style={styles.title}>My Todos</Text>
+      </View>
+      <View style={styles.content}>
+        {/* TODO FORM */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -46,17 +31,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
