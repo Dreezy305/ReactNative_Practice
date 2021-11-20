f18deae81ce4121547d027d29d6091e58bb6a98c
Idris Bankole <bankoleidris@gmail.com>
2021-11-20 21:14:28 +0100

    scroll view

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -9,38 +9,35 @@ export default function App() {
     { key: 3, name: "mario" },
     { key: 4, name: "luigi" },
     { key: 5, name: "android" },
+    { key: 6, name: "window" },
+    { key: 7, name: "mac" },
   ]);
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-    marginTop: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
+  item: {
+    marginTop: 24,
     padding: 30,
-    marginTop: 20,
-  },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
