7767182b8c9faaf6428c6d7b1261d41bdd2f1a27
Idris Bankole <bankoleidris@gmail.com>
2021-11-20 20:18:06 +0100

    multiline text input

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default function App() {
       <TextInput
         style={styles.input}
         value={text}
+        multiline
         placeholder="e.g. John doe"
         onChangeText={(val) => setText(val)}
       />
