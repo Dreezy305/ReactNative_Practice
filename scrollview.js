0b5ea96be807c94e7dd64bee5a52a03d32531d9a
Idris Bankole <bankoleidris@gmail.com>
2021-11-20 22:08:56 +0100

    scrollView

---
+++ scrollview.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+export default function scrollview() {
+  const [people, setPeople] = useState([
+    { key: 1, name: "shaun" },
+    { key: 2, name: "yoshi" },
+    { key: 3, name: "mario" },
+    { key: 4, name: "luigi" },
+    { key: 5, name: "android" },
+    { key: 6, name: "window" },
+    { key: 7, name: "mac" },
+  ]);
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+  },
+});
