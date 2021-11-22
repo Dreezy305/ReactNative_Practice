9854175bfec2bd0b56c593874a469e25f3678cbf
Idris Bankole <bankoleidris@gmail.com>
2021-11-22 23:24:27 +0100

    touchable without feedback

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import TodoItem from "./components/todoItem";
 import AddTodo from "./components/addTodo";
 
@@ -32,24 +40,26 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      {/* HEADER GOES HERE */}
-      <View style={styles.header}>
-        <Text style={styles.title}>My Todos</Text>
-      </View>
-      <View style={styles.content}>
-        {/* TODO FORM */}
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => console.log("dismissed")}>
+      <View style={styles.container}>
+        {/* HEADER GOES HERE */}
+        <View style={styles.header}>
+          <Text style={styles.title}>My Todos</Text>
+        </View>
+        <View style={styles.content}>
+          {/* TODO FORM */}
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
