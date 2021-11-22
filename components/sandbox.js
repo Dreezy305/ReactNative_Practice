f04f17c7282c84f867ad0e74a27a13465465e382
Idris Bankole <bankoleidris@gmail.com>
2021-11-23 00:28:33 +0100

    flex box

--- components/sandbox.js
+++ components/sandbox.js
@@ -16,24 +16,29 @@ const styles = StyleSheet.create({
   container: {
     // flex: 1,
     flexDirection: "row",
-    justifyContent: "flex-end",
+    justifyContent: "space-around",
+    alignItems: "flex-end",
     paddingTop: 40,
     backgroundColor: "#ddd",
   },
   boxOne: {
+    flex: 1,
     backgroundColor: "violet",
     padding: 10,
   },
   boxTwo: {
+    flex: 2,
     backgroundColor: "gold",
-    padding: 10,
+    padding: 20,
   },
   boxThree: {
+    flex: 1,
     backgroundColor: "coral",
-    padding: 10,
+    padding: 30,
   },
   boxFour: {
+    flex: 3,
     backgroundColor: "skyblue",
-    padding: 10,
+    padding: 40,
   },
 });
